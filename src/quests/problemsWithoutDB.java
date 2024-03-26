fd4dad97f60df1fc3f97b0565f33ce553c108181
rugkfl <kimgyoungha61@gmail.com>
2024-03-26 03:12:17 +0000

    UPDATE

--- src/quests/problemsWithoutDB.java
+++ src/quests/problemsWithoutDB.java
@@ -13,22 +13,21 @@ public class problemsWithoutDB {
         listProblems.add("3. 문제: Python에서 조건문을 작성하는 방법은? (점수: 10점)");
         listProblems.add("4. 문제: Python에서 함수를 정의하는 방법은? (점수: 5점)");
 
-        HashMap<String, String> hashOption = new HashMap<String, String>();
+        HashMap<Integer, String> hashOption = new HashMap<Integer, String>();
 
-        hashOption.put("답항","1) var name, 2) name = value, 3) set name, 4) name == value");
-        hashOption.put("답항", " 1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 상태로 유지된다");
-        hashOption.put("답항", "2) for-in, 3) while, 4) def");
-        hashOption.put("답항", "1) class, 2) def, 3) import, 4) return");
+        hashOption.put(1,"1) var name, 2) name = value, 3) set name, 4) name == value");
+        hashOption.put(2, " 1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 상태로 유지된다");
+        hashOption.put(3, "1) if-else, 2) for-in, 3) while, 4) def");
+        hashOption.put(4, "1) class, 2) def, 3) import, 4) return");
 
         System.out.println("아래는 4개의 Python 관련 문제와 각 문항의 난이도에 따른 점수화");
-        for (String problem : listProblems) {
-            for (String option : hashOption.values()) {
-                System.out.println(problem); 
-                System.out.println("답항 : "+option);
-                System.out.print("정답 : ");
-                String stranswer = scanner.nextLine();
-                break;
-                }
+        for (int i = 0; i < listProblems.size(); i++) {
+            String problems = listProblems.get(i);
+            System.out.println(problems);
+            String option = hashOption.get(i + 1);
+            System.out.println("답항 : "+option);
+            System.out.print("정답 : ");
+            String stranswer = scanner.nextLine();
             }
         scanner.close();   
     }
