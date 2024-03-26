7eb3bcb7db904dca2c17ce858961ee503fd51e1f
rugkfl <kimgyoungha61@gmail.com>
2024-03-26 03:21:02 +0000

    update

--- src/quests/problemsWithoutDB.java
+++ src/quests/problemsWithoutDB.java
@@ -15,7 +15,7 @@ public class problemsWithoutDB {
 
         HashMap<Integer, String> hashOption = new HashMap<Integer, String>();
 
-        hashOption.put(1,"1) var name, 2) name = value, 3) set name, 4) name == value");
+        hashOption.put(1, "1) var name, 2) name = value, 3) set name, 4) name == value");
         hashOption.put(2, " 1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 상태로 유지된다");
         hashOption.put(3, "1) if-else, 2) for-in, 3) while, 4) def");
         hashOption.put(4, "1) class, 2) def, 3) import, 4) return");
